ae527f15275532cc419c2845d560cd27181d5132
areph <40balmung@gmail.com>
2020-04-03 07:19:10 +0900

    learn compontnts

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer>Copyright {{getCurrentYear}} <a :href="href">Hello</a></footer>
+</template>
+
+<script>
+export default {
+    data: ()=> ({
+        href: 'https://google.co.jp'
+    }),
+    computed: {
+        getCurrentYear() {
+            return new Date().getFullYear();
+        }
+    }
+}
+</script>
+
+<style>
+    footer {
+        text-align: center;
+        color: blue;
+    }
+</style>
