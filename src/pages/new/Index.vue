fbb17e623a1264be6ee3fd62894b82506ae80fdf
areph <40balmung@gmail.com>
2020-04-03 07:16:25 +0900

    initial commit

---
+++ src/pages/new/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <Default>
+        <h1>Index</h1>
+    </Default>
+</template>
+<script>
+import Default from '~/layouts/Default.vue'
+export default {
+    components: {
+        Default
+    }
+}
+</script>
