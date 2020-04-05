39d197c71218cc5b6a74c4bca0964069a1eff4e1
areph <40balmung@gmail.com>
2020-04-06 07:12:21 +0900

    set meta info

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,5 +1,14 @@
 <template>
   <Layout>
     <h1>Products</h1>
-  </Layout>  
+  </Layout>
 </template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Products",
+    meta: [{ charaset: "utf-8" }, { name: "author", content: "Me!" }],
+  },
+};
+</script>
